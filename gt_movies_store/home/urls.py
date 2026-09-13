cbcd4201a03059060671c16563d0816b023abe63
Jun Hong <ong.junhong888@gmail.com>
2026-09-13 10:14:24 -0400

    replaced sotre with home; added base template

--- gt_movies_store/home/urls.py
+++ gt_movies_store/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('test/', views.test, name='test'),
+
 ]
--- gt_movies_store/home/views.py
+++ gt_movies_store/home/views.py
@@ -5,3 +5,7 @@ from django.template import loader
 def landing(request):
     template = loader.get_template('landing.html')
     return HttpResponse(template.render())
+
+def test(request):
+    template = loader.get_template('test.html')
+    return HttpResponse(template.render())
